f296312b4b3db769052ede34745c3df4d12fa92b
iam23db <iam23db@yandex.ru>
2022-08-08 22:38:07 +0300

    reorganize repositories

---
+++ Seminar_2/Example014/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+14. Напишите программу, которая принимает на вход число и 
+проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+string Division(int number, int div1, int div2)// Получаем ответом строку
+{
+    if (number % div1 == 0 & number % div2 == 0)
+    {
+        return "ДА";
+    }
+    else
+    {
+        return "НЕТ";
+    }
+}
+
+int number = new int();
+int div1 = 7;
+int div2 = 23;
+
+Console.WriteLine("введите число, ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+string divisionValue = Division(number, div1, div2);// получаем результат деления - строку да или нет
+Console.WriteLine(divisionValue);// выводим строку, которая получилась в функции и была возвращена return'ом.
---
+++ Seminar_2/Example016/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+16. Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+
+string IsASquare(int numberOne, int numberTwo)
+{
+    if (numberOne / numberTwo == numberTwo | numberTwo / numberOne == numberOne)
+    {
+        return "Да";
+    }
+    else
+    {
+        return "Нет";
+    }
+}
+Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+string isItASquare=IsASquare(numberOne, numberTwo);
+Console.WriteLine($"{isItASquare}, большее число - это квадрат меньшего числа");
