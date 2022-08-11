d8c55d09cc4810858e5cfb1ddd7b3dc1c99a75f9
iam23db <iam23db@yandex.ru>
2022-08-11 21:33:19 +0300

    seminar 3 added

---
+++ Seminar_3/Example017/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+int getQuarterFromCoordinate (int x, int y)
+{
+    int result = 0;
+    if(x>0 && y>0)
+    {
+        result = 1;
+    }
+    else if(x<0 && y>0)
+    {
+        result = 2;
+    }
+     else if (x<0 && y<0)
+    {
+        result = 3;
+    }
+    else if(x>0 && y<0)
+    {
+        result = 4;
+    }
+    else 
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine ($"X и Y не должны быть равны 0, а вы ввели {x} {y}");
+    }
+    return result;
+}
+int x = 0;
+int y = 0;
+Console.WriteLine("Введите X:");
+x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+y=Convert.ToInt32(Console.ReadLine());
+int quoter = getQuarterFromCoordinate(x,y);
+Console.WriteLine($"Для координаты {x}:{y} находится в {quoter} четверти");
---
+++ Seminar_3/Example018/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+
+Задача 18: Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+
+*/
+string getCoordinatesFromQuarter (int quarter)
+{ 
+    string availableQuarter = "Значения отсутсвуют, попробуйте другое значение";
+    if (quarter == 1)
+    {
+        availableQuarter =  "x>0 and y>0";
+    }
+    else if (quarter == 2)
+    {
+        availableQuarter =  "x<0 and y>0";
+    }
+    else if (quarter == 3)
+    {
+        availableQuarter =  "x<0 and y<0";
+    }
+    else if (quarter == 4)
+    {
+        availableQuarter =  "x>0 and y<0";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine ($"возможен подбор значений для одной из 4-х четвертей, а вы выбрали {quarter}");
+        Console.ResetColor();
+    }
+    return availableQuarter;
+}
+Console.WriteLine("Введите Четверть:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string answer = getCoordinatesFromQuarter(quarter);
+Console.WriteLine($"Возможные значения для выбранной четверти {answer}");
