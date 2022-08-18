2195a98ae4e3c443fd461263282a9d52aeca2c8b
iam23db <iam23db@yandex.ru>
2022-08-18 23:43:37 +0300

    added all seminar 5 examples

---
+++ Seminar_5/Example_031/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+/*
+
+Задайте масив из 12 элементов, заполните случайными числами из промежутка, 
+Зайдите сумму отрицательных и положительных элементов.
+
+*/
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{result[i]} ");
+    }
+    return result;
+}
+
+int[] array = GetArray(12, -9, 9);
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int i in array) // Для каждошо элемента (i) в (array)
+{
+    if (i > 0)
+        positiveSum += i;
+    else
+        negativeSum += i;
+}
+Console.WriteLine($"Сумма положительных = {positiveSum}, а сумма отрицительных = {negativeSum}" );
---
+++ Seminar_5/Example_032/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+*/
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{result[i]} ");
+    }
+    return result;
+}
+int[] ReverseArray(int[] array)
+{   int [] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+        Console.Write($"{array[i]} ");
+    }
+    return newArray;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine();
+ReverseArray(array);
