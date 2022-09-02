4bcb0a7bc68881d1ecf4d383a94a39126db5e2a7
iam23db <iam23db@yandex.ru>
2022-09-02 08:47:07 +0300

    last seminar - recursion

---
+++ Seminar_9/Example_063/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+
+
+
+string Number(int a, int b)
+{
+    if (a <= b)
+    {
+        return $"{a} " + Number(a + 1, b);
+    }
+    else
+    {
+        return string.Empty;
+    }
+}
+
+
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int a = 1;
+
+Console.WriteLine(Number (a,b));
---
+++ Seminar_9/Example_067/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+
+int SumOfDigits(int b)
+{
+    if (b == 0) return 0;
+    else return b % 10 + SumOfDigits(b / 10);
+}
+
+
+
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine(SumOfDigits(b));
